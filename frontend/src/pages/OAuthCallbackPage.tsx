5ed929cc4c8537bf472b325b4d904d648a333d9b
최석원 <seokwon1907@naver.com>
2026-03-18 11:33:46 +0900

    FE-refactor : 코드 수정

--- frontend/src/pages/OAuthCallbackPage.tsx
+++ frontend/src/pages/OAuthCallbackPage.tsx
@@ -1,6 +1,5 @@
 // 카카오 OAuth 로그인 후 리다이렉트되는 콜백 페이지
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 export default function OAuthCallbackPage() {
   useEffect(() => {
